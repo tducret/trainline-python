af28a7b5e77db746803e63331608e2117d68919c
Thibault Ducret <thibault.ducret@gmail.com>
2018-09-03 00:20:45 +0200

    Added stations_mini.csv to package_data to fix the 'stations_mini.csv does not exist' problem

--- setup.py
+++ setup.py
@@ -3,26 +3,28 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 try:  # For pip >= 10
     from pip._internal.req import parse_requirements
 except ImportError:  # For pip <= 9
     from pip.req import parse_requirements
 
 
-__version__ = '0.0.3'  # Should match with __init.py__
+__version__ = '0.0.4'  # Should match with __init.py__
+_PACKAGE_NAME = 'trainline'
 _GITHUB_URL = 'https://github.com/tducret/trainline-python'
 _KEYWORDS = ['api', 'trainline', 'parsing', 'train', 'sncf',
              'python-wrapper', 'scraping', 'scraper', 'parser']
 _SCRIPTS = ['trainline_cli.py']
+_PACKAGE_DATA = ['stations_mini.csv']
 
 install_reqs = parse_requirements('requirements.txt', session='hack')
 requirements = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name='trainline',
-    packages=find_packages(),
-    package_data={},
+    name=_PACKAGE_NAME,
+    packages=[_PACKAGE_NAME],
+    package_data={_PACKAGE_NAME: _PACKAGE_DATA, },
     scripts=_SCRIPTS,
     version=__version__,
     license="MIT license",
@@ -51,7 +53,7 @@ setup(
 # ------------------------------------------
 # Make sure everything was pushed (with a git status)
 # (or git commit --am "Comment" and git push)
-# export VERSION=0.0.3; git tag $VERSION -m "Added get_station_id and CLI tool"; git push --tags
+# export VERSION=0.0.4; git tag $VERSION -m "Fixed the 'stations_mini.csv does not exist' problem"; git push --tags
 
 # If you need to delete a tag
 # git push --delete origin $VERSION; git tag -d $VERSION
